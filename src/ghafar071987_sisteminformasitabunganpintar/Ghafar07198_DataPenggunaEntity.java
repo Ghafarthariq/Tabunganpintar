03fb5d20c1b1a8308770204beb8dab824a7e9533
Ghafarthariq <ghafarthariq.a@gmail.com>
2020-11-12 15:42:53 +0700

    test

---
+++ src/ghafar071987_sisteminformasitabunganpintar/Ghafar07198_DataPenggunaEntity.java
@@ -0,0 +1,22 @@
+package ghafar071987_sisteminformasitabunganpintar;
+public class Ghafar07198_DataPenggunaEntity {
+    String Ghafar07198_Email,Ghafar07198_Password,Ghafar07198_JenisKelamin,Ghafar07198_Nama;
+    public Ghafar07198_DataPenggunaEntity(String Ghafar07198_Email, String Ghafar07198_Password, String Ghafar07198_JenisKelamin, String Ghafar07198_Nama) {
+        this.Ghafar07198_Email = Ghafar07198_Email;
+        this.Ghafar07198_Password = Ghafar07198_Password;
+        this.Ghafar07198_JenisKelamin = Ghafar07198_JenisKelamin;
+        this.Ghafar07198_Nama = Ghafar07198_Nama;
+    }
+    public String getEmail() {
+        return Ghafar07198_Email;
+    }
+    public String getPassword() {
+        return Ghafar07198_Password;
+    }
+    public String getJenisKelamin() {
+        return Ghafar07198_JenisKelamin;
+    }
+    public String getNama() {
+        return Ghafar07198_Nama;
+    }
+}
